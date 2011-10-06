f50f0139cb9e159e18627d338c98e5dad35258d4
Damian Carrillo <dcarrillo@ironclad.mobi>
2011-10-06 15:01:38 -0500

    Initial commit for testing project

--- Car Lot/main.m
+++ Car Lot/main.m
@@ -1,18 +1,10 @@
-//
-//  main.m
-//  Car Lot
-//
-//  Created by Damian Carrillo on 10/6/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-#import "CLAppDelegate.h"
+#import "AppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
