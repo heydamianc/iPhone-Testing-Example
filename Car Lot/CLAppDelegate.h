688a01226e47b2aa1a9eba90086d140b6c20d365
Damian Carrillo <dcarrillo@ironclad.mobi>
2011-10-06 14:34:47 -0500

    Initial Commit

---
+++ Car Lot/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  Car Lot
+//
+//  Created by Damian Carrillo on 10/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Car LotTests/Car_LotTests.h
@@ -0,0 +1,13 @@
+//
+//  Car_LotTests.h
+//  Car LotTests
+//
+//  Created by Damian Carrillo on 10/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Car_LotTests : SenTestCase
+
+@end
